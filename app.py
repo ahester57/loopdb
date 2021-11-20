689be581d41b14e52960e660d3a05a88533010a7
Austin Hester <ahester57@gmail.com>
2021-11-20 23:23:38 +0000

    objectify App
    Signed-off-by: Austin Hester <ahester57@gmail.com>

--- app.py
+++ app.py
@@ -8,38 +8,63 @@ from redisconnector import RedisConnector
 from simple_server import LoopServer
 
 
-print(RedisConnector)
-redis_instance = RedisConnector()
-print(redis_instance)
-app = falcon.App()
+class App(object):
+    def __init__(self):
+        self._redis_instance = RedisConnector()
+        self._falcon_app = falcon.App()
+        self.create_resources()
+        self.create_routes()
+        self.create_server()
 
+    def create_resources(self):
+        self._loop = LoopResource(self._redis_instance)
+        self._dupe = DupeResource()
 
-loop = LoopResource(redis_instance)
-dupe = DupeResource()
+    def create_routes(self):
+        self._falcon_app.add_route('/loop', self._loop)
+        self._falcon_app.add_route('/dupe', self._dupe)
 
-app.add_route('/loop', loop)
-app.add_route('/dupe', dupe)
+    def create_server(self):
+        self._loop_server = LoopServer(self.get_falcon_app())
+
+    def get_falcon_app(self):
+        return self._falcon_app
+
+    def start_server(self):
+        self._loop_server.start()
+
+    def stop_server(self):
+        self._loop_server.stop()
+
+    def __repr__(self):
+        return f'''
+Redis: {self._redis_instance}
+Falcon: {self._falcon_app}
+Server: {self._loop_server}
+'''
+
 
 if __name__ == '__main__':
 
-    loop_server = LoopServer(app)
+    app = App()
 
     def signal_handler(signal_num=None, frame=None):
         print('looping out')
-        loop_server.stop()
+        app.stop_server()
         sys.exit(1)
 
     # Set the app to handle interrupt signal with our custom signal handler
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
 
-    print(f'boutta serve: {loop_server}')
-    loop_server.start()
-    print(f'Serving: {loop_server}')
+    print(f'\nboutta serve: {app}')
+    app.start_server()
+    print(f'Serving: {app}')
+
+    # wait here for CLI inputs
     try:
         while True:
             time.sleep(1)
     except:
         print('looping out on accident')
-        loop_server.stop()
-
+        app.stop_server()
